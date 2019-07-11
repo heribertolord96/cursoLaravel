fe49f58dd74c9a5fe01dcdcc0fdeaab745e199ec
heribertolord96 <heribertolord@gmail.com>
2019-07-11 12:31:38 -0500

    Repositoriohasta migracionespart2

---
+++ database/migrations/2019_07_11_171419_crear_tabla_usuario_rol.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrearTablaUsuarioRol extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()//Tabla usuario_rol
+    {
+        Schema::create('usuario_rol', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('rol_id');
+            $table->foreign('rol_id','fk_usuario_rol')
+                ->references('id')
+                ->on('rol')
+                ->onDelete('restrict')
+                ->onUpdate('restrict');
+
+        $table->foreign('usuario_id','fk_usuario_usuario')
+                ->references('id')
+                ->on('usuario')
+                ->onDelete('restrict')
+                ->onUpdate('restrict');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuario_rol');
+    }
+}
---
+++ database/migrations/2019_07_11_172622_crear_tabla_libro.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrearTablaLibro extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('libro', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('titulo',50);
+            $table->string('isbn',30);
+            $table->string('autos',100);
+            $table->unsignedTinyInteger('cantidad');
+            $table->string('editorial',50)->nullable();
+            $table->string('foto',130)->nullable();
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('libro');
+    }
+}
